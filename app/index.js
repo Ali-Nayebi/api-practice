8ffd52cc5cd283aff7814b5dbe101d3f78cc1ac7
Ali Nayebi <elderekkoo@gmail.com>
2023-07-26 13:06:48 +0330

    adding users array and errors

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import http from "http";
+let users = [];
 const app = express();
 function App() {
   const server = http.createServer(app);
@@ -12,24 +13,47 @@ function App() {
   });
 
   app.post("/signup", (req, res) => {
-    var errors = [];
-    var pattern = /^[a-z0-9]{1,}@[a-z0-9]{1,}\.[a-z]{1,}$/i;
-    if (!pattern.test(req.body.email) || req.body.password.length < 6) {
-      if (!pattern.test(req.body.email)) {
+    try {
+      var errors = [];
+      var pattern = /^[a-z0-9]{1,}@[a-z0-9]{1,}\.[a-z]{1,}$/i;
+      if (!pattern.test(req.body.email) || req.body.password.length < 6) {
+        if (!pattern.test(req.body.email)) {
+          errors.push({
+            key: "email",
+            errorText: "فرمت ایمیل وارد شده صحیح نمی باشد",
+          });
+        }
+        if (req.body.password.length < 6) {
+          errors.push({
+            key: "password",
+            errorText: "رمز عبور وارد شده حداقل باید 6 کاراکتر باشد",
+          });
+        }
+        res.status(400).json({ errors: errors });
+      }
+      var err = false;
+      users.forEach((user) => {
+        if (user.email === req.body.email) {
+          err = true;
+        }
+      });
+      if (err) {
         errors.push({
           key: "email",
-          errorText: "فرمت ایمیل وارد شده صحیح نمی باشد",
+          errorText: "با این ایمیل قبلا ثبت نام انجام شده",
         });
+        res.status(400).json({ errors: errors });
       }
-      if (req.body.length < 6) {
-        errors.push({
-          key: "password",
-          errorText: "رمز عبور وارد شده حداقل باید 6 کاراکتر باشد",
-        });
-      }
-      res.status(400).json({ errors: errors });
+      users.push({
+        email: req.body.email,
+        password: req.body.password,
+        token: "",
+      });
+      console.log(users);
+      res.status(201).json(req.body);
+    } catch (e) {
+      res.status(500).json({});
     }
-    res.status(201).json(req.body);
   });
 
   server.listen(3000, () => {
