39fa8efde07bab6b408f8725fb3b0f87db7a20a0
Ali Nayebi <elderekkoo@gmail.com>
2023-08-02 13:38:47 +0330

    client side getprofile api

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,9 +1,42 @@
-import React from 'react'
+/* eslint-disable react-hooks/exhaustive-deps */
+import { Button } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Profile() {
+  const [email, setEmail] = useState();
+  const navigate = useNavigate();
+  useEffect(() => {
+    let status;
+    fetch("http://localhost:3001/profile", {
+      method: "GET",
+      headers: {
+        Authorization: localStorage.token_type + " " + localStorage.token,
+      },
+    })
+      .then((response) => {
+        status = response.status;
+        return response.json();
+      })
+      .then((responseJson) => {
+        if (status === 401) {
+          navigate("/login");
+        } else if (status === 200) {
+          setEmail(responseJson.user.email);
+        }
+      });
+  }, []);
   return (
     <div>
-      profile
+      {email}
+      <Button
+        onClick={() => {
+          localStorage.clear();
+          window.location.reload();
+        }}
+      >
+        logout
+      </Button>
     </div>
-  )
+  );
 }
